ca60d8ac59d033cae8a9cbb7c1657ea43bfecfc7
xionggen <xionggen@vanje.com.cn>
2019-09-11 13:17:05 +0800

    增加Button组件

---
+++ app/src/components/AppButton.vue
@@ -0,0 +1,60 @@
+<template>
+    <button
+        class="app-button" 
+        :class="{'app-button-text': type === 'text', 'app-button-primary': type === 'primary', 'app-button-plain': type === 'plain', 'app-button-block': block}"
+        @click="handleClick">
+        {{text}}
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'app-button',
+    props: {
+        text: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            default: 'plain',
+        },
+        block: {
+            type: Boolean,
+            default: false,
+        },
+        click: {
+            type: Function
+        }
+    },
+    methods: {
+        handleClick() {
+            this.$emit('click');
+        }
+    }
+}
+</script>
+
+<style lang="less">
+    @import '../assets/styles/theme.less';
+    .app-button {
+        padding: 6px 14px;
+        border: 1px solid transparent;
+        border-radius: 2px;
+    }
+    .app-button-text {
+        color: @theme-color;
+    }
+    .app-button-primary {
+        color: #fff;
+        background: @theme-color;
+    }
+    .app-button-plain {
+        color: @theme-color;
+        border-color: @theme-color;
+    }
+    .app-button-block {
+        width: 100%;
+        box-sizing: border-box;
+    }
+</style>
--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import './assets/styles/global.less';
+import './assets/styles/theme.less';
 
 Vue.config.productionTip = false
 
